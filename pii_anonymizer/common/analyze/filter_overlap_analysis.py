986313f1e582f5686e9ae9a28264dbba5a51e338
Pee Tankulrat <{ID}+{username}@users.noreply.github.com>
2022-12-07 17:00:52 +0700

    Filter overlap detection

--- pii_anonymizer/common/analyze/filter_overlap_analysis.py
+++ pii_anonymizer/common/analyze/filter_overlap_analysis.py
@@ -11,24 +11,28 @@ def filter_overlapped_analysis(analyzer_results: List[AnalyzerResult]):
     index_to_remove = set()
 
     for a in range(len(analyzer_results)):
-        for b in range(a + 1, len(analyzer_results)):
-            result_a = analyzer_results[a]
-            result_b = analyzer_results[b]
-            if result_b.start <= result_a.end or result_b.end >= result_a.start:
-                # Overlap
-                a_distance = result_a.end - result_a.start
-                b_distance = result_b.end - result_b.start
-                if a_distance > b_distance:
-                    index_to_remove.add(b)
-                elif a_distance < b_distance:
+        if a == len(analyzer_results) - 1:
+            break
+        result_a = analyzer_results[a]
+        result_b = analyzer_results[a + 1]
+        if (
+            result_a.start <= result_b.start <= result_a.end
+            or result_a.start <= result_b.end <= result_a.end
+        ):
+            # Overlap, pick longest match, then lower precendence
+            a_distance = result_a.end - result_a.start
+            b_distance = result_b.end - result_b.start
+            if a_distance > b_distance:
+                index_to_remove.add(a + 1)
+            elif a_distance < b_distance:
+                index_to_remove.add(a)
+            else:
+                result_a_precedence = DETECTOR_PRECEDENCE[result_a.type]
+                result_b_precedence = DETECTOR_PRECEDENCE[result_b.type]
+                if result_a_precedence < result_b_precedence:
+                    index_to_remove.add(a + 1)
+                elif result_a_precedence > result_b_precedence:
                     index_to_remove.add(a)
-                else:
-                    result_a_precedence = DETECTOR_PRECEDENCE[result_a.type]
-                    result_b_precedence = DETECTOR_PRECEDENCE[result_b.type]
-                    if result_a_precedence < result_b_precedence:
-                        index_to_remove.add(b)
-                    elif result_a_precedence > result_b_precedence:
-                        index_to_remove.add(a)
 
     sorted_index = sorted(index_to_remove, reverse=True)
     for index in sorted_index:
