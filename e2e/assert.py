0b3e15898987ecf9313d839da4a3ba228b8d9aaa
Pee Tankulrat <{ID}+{username}@users.noreply.github.com>
2022-12-14 15:04:50 +0700

    Add E2E to path filter

--- e2e/assert.py
+++ e2e/assert.py
@@ -6,16 +6,15 @@ def standalone_should_match_snapshot():
     expect = pd.read_csv("./e2e/expected.csv")
     actual = pd.read_csv("./e2e/output/standalone.csv")
     pd.testing.assert_frame_equal(expect, actual)
+    print("E2E - Standalone test passed")
 
 
 def spark_should_match_snapshot():
     expect = pd.read_csv("./e2e/expected.csv")
     actual = dd.read_csv("./e2e/output/spark/*.csv").compute()
     pd.testing.assert_frame_equal(expect, actual)
+    print("E2E - Spark test passed")
 
 
 standalone_should_match_snapshot()
-print("E2E - Standalone test passed")
-
 spark_should_match_snapshot()
-print("E2E - Spark test passed")
